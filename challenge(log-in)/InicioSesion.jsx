cae317879043e0458e160b3a59cb6ccc38b14546
tatiespi <tatiana.espinosa@uao.edu.co>
2023-04-19 18:53:54 -0500

    fake log in

---
+++ challenge(log-in)/InicioSesion.jsx
@@ -0,0 +1,28 @@
+import "../../pages/styles/inicio_sesion.css";
+
+import ImgRef from "/Users/tatianaespinosa/my-app/Challenges/imagenes/sol.jpg";
+import InputLogin from "../../shared/components/InputLogin";
+
+function InicioSesion() {
+  return (
+    <div className="Area_login">
+      <div className="Barra_Inicio">
+
+      </div>
+      <div className="todo-menos-header">
+        {/* aqui tenes que hacer divs para la zona de la imagen, otro div con el input */}
+        <div className="Imagen">
+          <img className="img-img-ref " alt="img de ref" src={ImgRef}></img>
+        </div>
+
+        {/* <a>WELCOME</a> */}
+        <div className="Ingresar_datos">
+          <h1>BIENVENIDO</h1>
+          <InputLogin />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InicioSesion;
---
+++ challenge(log-in)/InputLogin.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import usersData from "../../jsonusers/users.json";
+import { Button, Checkbox, Form, Input } from "antd";
+import { useNavigate } from "react-router-dom";
+
+const InputLogin = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Buscamos el usuario en el archivo JSON
+    const user = usersData.users.find((u) => u.username === username);
+
+    console.log(user);
+
+    if (!user) {
+      setErrorMessage("Usuario no encontrado");
+    } else if (user.password !== password) {
+      setErrorMessage("Contraseña incorrecta");
+    } else {
+      setErrorMessage("");
+      navigate("/");
+    }
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      style={{
+        width: "auto",
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        value={username}
+        type="text"
+        onChange={(event) => setUsername(event.target.value)}
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        type="password"
+        value={password}
+        className="form-item-password"
+        onChange={(event) => setPassword(event.target.value)}
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="link">¿Olvidó su contraseña?</Button>
+        <Button className="btn_ingresar" onClick={handleSubmit}>
+          Ingresar
+        </Button>
+      </Form.Item>
+      {errorMessage && <p>{errorMessage}</p>}
+    </Form>
+  );
+};
+
+export default InputLogin;
